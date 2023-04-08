08cb5b1d358356afb09a38550203db2018c2a8d8
aliawilkinson <aliawilkinson@gmail.com>
2023-04-07 19:56:05 -0700

    Video at 33:34

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,27 @@
 import React from "react"
 import css from './Header.module.scss'
+import {BiPhoneCall} from 'react-icons/bi'
 
 const Header = () => {
     return (
-        <div>Header</div>
+        <div className={`paddings ${css.wrapper}`}>
+            <div className={`flexCenter innerWidth ${css.container}`}>
+                <div className={css.name}>
+                    Alia
+                </div>
+
+                <ul className={`flexCenter ${css.menu}`}>
+                    <li><a href="">Services</a></li>
+                    <li><a href="">Experience</a></li>
+                    <li><a href="">Portfolio</a></li>
+                    <li><a href="">Testimonials</a></li>
+                    <li>
+                        <p>+1 892 892 8392</p>
+                        <BiPhoneCall size={"40px"}/>
+                    </li>
+                </ul>
+            </div>
+        </div>
     )
 }
 
