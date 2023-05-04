a5c5781340163b56483ef451e846cd56c061d2b5
aliawilkinson <aliawilkinson@gmail.com>
2023-05-04 17:23:44 -0600

    hero v1 50:55

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import css from './hero.module.scss'
+
+const Hero = () => {
+    return (
+        <section className={css.wrapper}>
+            Hero
+        </section>
+    )
+}
+
+export default Hero
