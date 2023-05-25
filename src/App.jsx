6abc98844a3228d2c14a9a8ec92c843f9ef14340
aliawilkinson <aliawilkinson@gmail.com>
2023-05-24 18:13:51 -0600

    review carousel v1

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from './components/Hero/Hero'
 import Expertise from './components/Expertise/Expertise'
 import Experience from './components/Experience/Experience'
 import Portfolio from './components/Portfolio/Portfolio'
+import Reviews from './components/Reviews/Reviews'
 
 const App = () => {
   return <div className={`bg-primary ${css.container}`}>
@@ -12,6 +13,7 @@ const App = () => {
     <Expertise/>
     <Experience/>
     <Portfolio/>
+    <Reviews/>
   </div>
 };
 
