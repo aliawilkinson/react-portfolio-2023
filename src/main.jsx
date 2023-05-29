67ff2fc541c8241793056394c083786d09d3bdd2
aliawilkinson <aliawilkinson@gmail.com>
2023-05-29 15:23:11 -0600

    Fixed routing to about page

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import ScrollToTop from 'react-scroll-to-top'
 import App from './App'
 import './styles/index.css'
 import './styles/global.scss'
